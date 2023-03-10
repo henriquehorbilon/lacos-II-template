cb00b072028437577f8880531ca61c3daba49146
Henrique Horbilon <henriquehorbilon@gmail.com>
2023-03-10 18:14:11 -0300

    lista template lacos feito em aula

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
 // --------------- PRÁTICA GUIADA ---------------
 
 
+//const bimestre1 = [10, 10, 10, 10] // soma = 40 ; media = 10
+//const bimestre2 = [8, 5, 7.5, 3]
+//const bimestre3 = [6, 7, 8.5, 10]
+//const bimestre4 = [3, 5, 2, 0]
+
+//const notasDoAno = [bimestre1, bimestre2, bimestre3, bimestre4]
+
+// for (let i = 0; i < notasDoAno.length; i++) {
+//   const bimestreDaVez = notasDoAno[i]
+
+//   let soma = 0
+//   let media = 0
+
+//   // construindo laço para percorrer cada apartamento do andar
+//   for (let j = 0; j < bimestreDaVez.length; j++) {
+//     const nota = bimestreDaVez[j]
+//     soma = soma + nota
+//   }
+
+//   media = soma / bimestreDaVez.length
+
+//   console.log(`A soma das notas do bimestre ${[i + 1]} é ${soma} \nA média das notas do bimestre ${[i + 1]} é ${media}`)
+// }
 
 
 
+for (let indice in notasDoAno) {
+  indice = Number(indice)
 
+  let soma = 0
+  let media = 0
 
+  for (let elemento of notasDoAno[indice]) {
+    soma = soma + elemento
+  }
+  media = soma / notasDoAno[indice].length
 
-
-
-
-
-
-
+  console.log(`A soma das notas do bimestre ${indice + 1} é ${soma} \nA média das notas do bimestre ${indice + 1} é ${media}`)
+}
 
 
 
@@ -56,4 +83,39 @@ const filmes = [
   },
 ]
 
+for (let indice in filmes) {
+  const filme = filmes[indice]
+  const titulo = filme.titulo
+  const ano = filme.ano
+  const diretor = filme.diretor
+  const elenco = filme.elenco
+
+  const frase = `${titulo}, de ${ano}, dirigido por ${diretor}.`
+  console.log(frase)
+
+  for (let artista of elenco) {
+    const indiceArtista = elenco.indexOf(artista)
+
+    const fraseElenco = `Atriz/Ator ${indiceArtista + 1}: ${artista}`
+
+    console.log(fraseElenco)
+  }
+}
+
 // escreva seu código abaixo 👇🏻
+
+
+// for (let i = 0; i < filmes.length; i++) {
+//   const filme = filmes[i]
+//   const elenco = filme.elenco
+
+//   const frase = `${filme.titulo}, de ${filme.ano}, dirigido por ${filme.diretor}.`
+
+//   console.log(frase)
+
+//   for (let j = 0; j < elenco.length; j++) {
+//     const fraseElenco = `Atriz/Ator ${j + 1}: ${elenco[j]}`
+
+//     console.log(fraseElenco)
+//   }
+// }
